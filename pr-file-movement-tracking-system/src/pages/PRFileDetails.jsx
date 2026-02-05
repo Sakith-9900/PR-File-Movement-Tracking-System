b0759667280f9e8e49abed5839f63e09376f6b49
Sakith Gunarathna <sakithmandira99@gmail.com>
2026-02-05 19:33:44 +0530

    Update PRFileDetails.jsx

--- pr-file-movement-tracking-system/src/pages/PRFileDetails.jsx
+++ pr-file-movement-tracking-system/src/pages/PRFileDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { base44 } from "@/api/base44Client";
+import { base44 } from "@/api/Client";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { format } from "date-fns";
