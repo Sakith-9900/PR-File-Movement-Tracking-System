4d06347f8324290f03fb6924bc547d46cda399b6
Sakith Gunarathna <sakithmandira99@gmail.com>
2026-02-07 23:21:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import { AuthProvider } from './contexts/authcontext';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Wrap your app
+<AuthProvider>
   <App />
-)
+</AuthProvider>
